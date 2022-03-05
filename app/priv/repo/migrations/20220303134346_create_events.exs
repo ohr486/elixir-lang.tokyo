37b325ae6b5b03272ea8632ae36ef79229a86b2d
ohr486 <ohr486@gmail.com>
2022-03-06 01:27:12 +0900

    add on_delete

--- app/priv/repo/migrations/20220303134346_create_events.exs
+++ app/priv/repo/migrations/20220303134346_create_events.exs
@@ -3,11 +3,13 @@ defmodule ElixirLangTokyo.Repo.Migrations.CreateEvents do
 
   def change do
     create table(:events) do
-      add :meetup_id, references(:meetups)
+      add :meetup_id, references(:meetups, on_delete: :nothing)
       add :name, :string
       add :url, :string
 
       timestamps()
     end
+
+    create index(:events, [:meetup_id])
   end
 end
--- app/priv/repo/migrations/20220303134451_create_talks.exs
+++ app/priv/repo/migrations/20220303134451_create_talks.exs
@@ -3,12 +3,14 @@ defmodule ElixirLangTokyo.Repo.Migrations.CreateTalks do
 
   def change do
     create table(:talks) do
-      add :event_id, references(:events)
+      add :event_id, references(:events, on_delete: :nothing)
       add :alchemist_id, :integer
       add :title, :string
       add :slide, :string
 
       timestamps()
     end
+
+    create index(:talks, [:event_id])
   end
 end
