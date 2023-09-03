e916b9888fb3103a77ade6a219c554e8185f780b
ohr486 <ohr486@gmail.com>
2023-09-03 16:37:43 +0900

    update aws

--- infra/aws/backend.tf
+++ infra/aws/backend.tf
@@ -1,10 +1,23 @@
+provider "aws" {
+  region                  = "ap-northeast-1"
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = "elixir-lang-tokyo-terraform" # SET YOUR PROFILE
+}
+
 terraform {
   backend "s3" {
-    bucket                  = "ohr486.terraform"          # SET YOUR BUCKET
-    key                     = "elixir-lang-tokyo.tfstate" # SET YOUR KEY
+    bucket                  = "ohr486.terraform"            # SET YOUR BUCKET
+    key                     = "elixir-lang-tokyo.tfstate"   # SET YOUR KEY
     region                  = "ap-northeast-1"
-    dynamodb_table          = "tfstate"                   # SET YOUR DDB TABLE
+    dynamodb_table          = "tfstate"                     # SET YOUR DDB TABLE
     shared_credentials_file = "~/.aws/credentials"
-    profile                 = "ohr486base-terraform"      # SET YOUR PROFILEt
+    profile                 = "elixir-lang-tokyo-terraform" # SET YOUR PROFILEt
+  }
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.44"
+    }
   }
 }
